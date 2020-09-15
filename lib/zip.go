128c575f1357e657c515000abce0c68f9e582458
rishichawda <chawda.rishii@gmail.com>
2020-09-15 17:17:37 +0530

    fix(zip): failed extraction from created archive

--- lib/zip.go
+++ lib/zip.go
@@ -2,7 +2,6 @@ package corelib
 
 import (
 	"archive/zip"
-	"compress/flate"
 	"io"
 	"log"
 	"os"
@@ -11,7 +10,7 @@ import (
 	"time"
 )
 
-func CreateArchive(file_path string, compressionLevel int) {
+func CreateArchive(file_path string, noCompress bool) {
 	start := time.Now()
 	_, input_filename := filepath.Split(file_path)
 	output_file, err := os.Create(input_filename + ".zip")
@@ -22,15 +21,18 @@ func CreateArchive(file_path string, compressionLevel int) {
 	LogIfError(err, true, func ()  {})
 	file_writer := zip.NewWriter(output_file)
 	defer file_writer.Close()
-	file_writer.RegisterCompressor(zip.Deflate, func(w io.Writer) (io.WriteCloser, error) {
-		return flate.NewWriter(w, compressionLevel)
-	})
 	filepath.Walk(file_path, func (path string, file_info os.FileInfo, err error) error {
 		if !file_info.IsDir() {
 			file_reader, err := os.Open(path)
-			defer file_reader.Close()
 			LogIfError(err, true, cleanup)
-			created_file, err := file_writer.Create(path)
+			defer file_reader.Close()
+			file_info, err := file_reader.Stat()
+			LogIfError(err, true, cleanup)
+			file_header, err := zip.FileInfoHeader(file_info)
+			LogIfError(err, true, cleanup)
+			file_header.Name = strings.TrimPrefix(path, input_filename + "/")
+			file_header.Method = map[bool]uint16{true: zip.Store, false: zip.Deflate}[noCompress]
+			created_file, err := file_writer.CreateHeader(file_header)
 			LogIfError(err, true, cleanup)
 			_, err = io.Copy(created_file, file_reader)
 			LogIfError(err, true, cleanup)
@@ -46,17 +48,20 @@ func ExtractZip(file_path string)  {
 	cleanup := func() {
 		os.RemoveAll(output_dirname)
 	}
-	LogIfError(os.Mkdir(output_dirname, os.ModePerm), true, cleanup)
+	LogIfError(os.MkdirAll(output_dirname, os.ModePerm), true, cleanup)
 	reader, err := zip.OpenReader(file_path)
-	defer reader.Close()
 	LogIfError(err, true, cleanup)
+	defer reader.Close()
 	for _, file := range reader.File {
 		output_filepath := filepath.Join(output_dirname, file.Name)
 		log.SetPrefix("Extracting: ")
 		log.Println(output_filepath)
 		if file.FileInfo().IsDir() {
-			os.Mkdir(filepath.Join(output_dirname, file.Name), os.ModePerm)
+			os.MkdirAll(filepath.Join(output_dirname, file.Name), os.ModePerm)
 		} else {
+			// mkdirall was required to create directory before creating the file. Other zips were extracted earlier without any errors because they preserved order.
+			// files created with this package don't preserve order, hence there are scenarios where directory is not present / already created before a contained file is written.
+			os.MkdirAll(filepath.Dir(output_filepath), os.ModePerm)
 			file_reader, err := file.Open()
 			defer file_reader.Close()
 			LogIfError(err, true, cleanup)
--- main.go
+++ main.go
@@ -11,20 +11,26 @@ var shouldExtract *bool
 var source *string
 var level *int
 var useGZip *bool
+var storageOnly *bool
 
 func readFlags()  {
 	shouldExtract = flag.Bool("extract", false, "Boolean to indicate if the file needs be extracted. By default, it will try to compress.")
 	useGZip = flag.Bool("gzip", false, "Use GZip compression")
 	source = flag.String("src", "", "Source file path (Required)")
+	storageOnly = flag.Bool("store", false, "Don't compress zip file.")
 	level = flag.Int("level", 9, "Compression level. 0 -> Storage, 9 -> Best Compression. (Defaults to 9)")
 	flag.Parse()
 	if *source == "" {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
+	if(*useGZip && *storageOnly) {
+		log.Println("Invalid option -store. GZip doesn't support store flag.")
+		os.Exit(1)
+	}
 	isLevelWithinThreshold := *level == 1 || *level == 9
 	if !isLevelWithinThreshold {
-		log.Println("Error: Invalid values for compression level. Should be between 0 (storage) and 9 (best).")
+		log.Println("Error: Invalid values for compression level. Should be 0 (storage) or 9 (best).")
 		os.Exit(1)
 	}
 }
@@ -43,7 +49,7 @@ func main()  {
 		if *useGZip {
 			corelib.CreateGZip(*source, *level)
 		} else {
-			corelib.CreateArchive(*source, *level)
+			corelib.CreateArchive(*source, *storageOnly)
 		}
 	}
 }
